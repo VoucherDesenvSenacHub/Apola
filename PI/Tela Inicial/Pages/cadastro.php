19074a9383f47a4510bcad860fa50e0c6954948d
Guilherme De Miranda Cardoso <guilhermemiranda1080@gmail.com>
2024-10-25 09:23:16 -0400

    alteração

--- PI/Tela Inicial/Pages/cadastro.php
+++ PI/Tela Inicial/Pages/cadastro.php
@@ -1,3 +1,19 @@
 <?php
-var_dump($_POST);
-echo "esta funcionando";
+include '../Pages/conecta.php';
+// isset -> significa está setado?? existe?? 
+if (isset($_POST)) {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $cep = $_POST['cep'];
+    $senha = $_POST['senha'];
+    
+    $sql = "INSERT INTO cliente (nome,email,cpf,cep,senha) VALUES ('$nome','$email','$cpf','$cep','$senha')";
+    $res = mysqli_query($con,$sql);
+    if($res){
+        echo "cadastro com sucesso ";
+    }else{
+        echo "ERRROOOOOOOOO";
+    }
+
+}
