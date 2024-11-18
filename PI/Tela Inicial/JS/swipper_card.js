1de89e34289c6172167ebe8c826d6c9a27b31a02
DEVR01001 <rafael.dev01001@gmail.com>
2024-11-18 10:19:57 -0400

    card_produto

---
+++ PI/Tela Inicial/JS/swipper_card.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".btn_next_card",
+    prevEl: ".btn_prev_card",
+  },
+
+  breakpoints: {
+    // when window width is >= 320px
+    520: {
+      slidesPerView: 1,
+      spaceBetween: 25
+    },
+
+    661: {
+      slidesPerView: 2,
+      spaceBetween: 25
+
+    },
+    // when window width is >= 480px
+    985: {
+      slidesPerView: 3,
+      spaceBetween: 25
+    },
+    // when window width is >= 640px
+    1220: {
+      slidesPerView: 3,
+      spaceBetween: 25
+    },
+
+    1420: {
+      slidesPerView: 4,
+      spaceBetween: 40
+
+    }
+  }
+
+
+});
