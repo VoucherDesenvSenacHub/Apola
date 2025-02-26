a7abd42a5da790c9d5d1bc18585518a5a354f670
DEVR01001 <rafael.dev01001@gmail.com>
2025-02-26 07:47:42 -0400

    at carrinho

--- PI/src/JS/drop_cep.js
+++ PI/src/JS/drop_cep.js
@@ -6,12 +6,16 @@ const OpenBtnradio2 = document.getElementById('radio_cep2');
 
 OpenBtnradio.addEventListener("click", () =>{
     ConteudoBtnradio.classList.add("active_drop_cep");
+    OpenBtnradio2.style.background = "transparent";
+    OpenBtnradio.style.background = "#FA301E";
 });
 
 
 
 OpenBtnradio2.addEventListener("click", ()=>{
     ConteudoBtnradio.classList.remove("active_drop_cep")
+    OpenBtnradio.style.background = "transparent";
+    OpenBtnradio2.style.background = "#FA301E";
 })
 
 
