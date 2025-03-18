6ad39cf248fd27ab83fe592916bd21bd809b1976
lourdesdecdic <lourdesdecdic@gmail.com>
2025-03-18 08:47:11 -0400

    Adicionei comentario do usuario e documentei o código

--- PI/Pages/user/SobreNos.php
+++ PI/Pages/user/SobreNos.php
@@ -6,17 +6,17 @@ include "navbar_deslogado.php";
 ?>
     <main style=" margin-bottom: unset;"  class="main2">
 
-        <section class="Banner_Sobre_Nos">
-            <div class="shape_sobre"></div>
-            <div class="content_banner_sobre_nos">
-                <img class="img_sobre_banner" src="../../src/imagens/SobreMim/freepik-export-20241209130412wAw2.jpeg" alt="">
+        <!-- <section class="Banner_Sobre_Nos"> -->
+            <!-- <div class="shape_sobre"></div> -->
+            <!-- <div class="content_banner_sobre_nos"> -->
+                <!-- <img class="img_sobre_banner" src="../../src/imagens/SobreMim/freepik-export-20241209130412wAw2.jpeg" alt="">
                 <div class="content_coteudo_banner_sobre">
                     <h4 class="text_top_sobre">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
                     <h5 class="text_bottom_sobre">Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h4>
                     <button class="btn_sobre"> <a href="">Conheça</a></button>
                 </div>
             </div>
-        </section>
+        </section> -->
 
 
 
@@ -26,28 +26,39 @@ include "navbar_deslogado.php";
                     <img src="../../src/imagens/login/asdfghjk.png" alt="">
                 </div>
                 <div class="left_about_text">
-                    <div class="title_text_about">Lorem ipsum <span class="title_text_about2">dolor sit.</span></div>
-                    <div class="conteudo_text_about">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur atque tenetur non officia repellendus blanditiis cum voluptatum velit perspiciatis earum, sed totam iste recusandae temporibus similique rem corrupti sunt repellat! <br> <br>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, sed aut dolore quibusdam, quos quasi modi veritatis aliquid cumque accusamus possimus totam aspernatur ipsum ad enim impedit soluta dignissimos ipsam!
+                    <div class="title_text_about"> Sobre nós <span class="title_text_about2"> Apola Ecommerce Artesanatos </span></div>
+                    <div class="conteudo_text_about">
+
+                        <p class="texto_about">
+                        A Apola Ecommerce Artesanatos nasceu para conectar artesãos talentosos a um público que valoriza a autenticidade e o trabalho manual.
+                        Nossa loja virtual oferece uma curadoria especial de produtos artesanais, incluindo itens decorativos, joias, roupas, acessórios e utilidades domésticas, todos feitos com dedicação e qualidade por artesãos locais e regionais.
+                        Nosso propósito é ampliar a visibilidade do artesanato, promovendo a cultura e incentivando o consumo consciente.
+                        Trabalhamos para garantir uma experiência de compra intuitiva, segura e satisfatória, com um catálogo detalhado, fotos de alta qualidade e um checkout simplificado. Além disso, proporcionamos um ambiente onde artesãos podem gerenciar seus estoques e pedidos com facilidade.
+                        </p>
+
+                        <p class="texto_about">
+                        Acreditamos que cada peça conta uma história e queremos levá-las até você. Siga-nos nas redes sociais e faça parte dessa comunidade apaixonada por arte e criatividade. Juntos, valorizamos o feito à mão!
+                        </p>
+                    
                     </div>
                     <div class="content_number_dados_sobre">
                         <div class="couteudo_dados_sobre">
                             <div class="number_dados_sobre">28+</div>
-                            <div class="text_dados_sobre">Lorem ipsum dolor sit</div>
+                            <div class="text_dados_sobre">Apola Ecommerce Artesanatos</div>
                         </div>
                         <div class="couteudo_dados_sobre">
                             <div class="number_dados_sobre">894+</div>
-                            <div class="text_dados_sobre">Lorem ipsum dolor sit</div>
+                            <div class="text_dados_sobre">Apola Ecommerce Artesanatos</div>
                         </div>
                         <div class="couteudo_dados_sobre">
                             <div class="number_dados_sobre">42+</div>
-                            <div class="text_dados_sobre">Lorem ipsum dolor sit</div>
+                            <div class="text_dados_sobre">Apola Ecommerce Artesanatos</div>
                         </div>
 
                     </div>
-                    <div class="content_btn_about">
+                    <!-- <div class="content_btn_about">
                         <button class="btn_about">Confira</button>
-                    </div>
+                    </div> -->
                 </div>
 
             </div>
@@ -56,7 +67,7 @@ include "navbar_deslogado.php";
 
 
 
-        <section class="cards_service">
+        <!-- <section class="cards_service">
             <div class="content_cards_service">
                 <h1 class="title_section_sobre">
                     Lorem ipsum dolor sit <span class="title_section_sobre2">amet consectetur</span>
@@ -86,7 +97,7 @@ include "navbar_deslogado.php";
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
 
 
 
@@ -94,7 +105,7 @@ include "navbar_deslogado.php";
         <section class="card_avaliacao_sobre">
             <div class="content_card_avalicao_sobre">
                 <h1 class="title_section_sobre">
-                    Lorem ipsum dolor sit <span class="title_section_sobre2">amet consectetur</span>
+                Apola Ecommerce Artesanatos <span class="title_section_sobre2"> </span>
                 </h1>
                 <div class="swiper_sobre">
                     <div class="swiper-wrapper">
@@ -110,8 +121,8 @@ include "navbar_deslogado.php";
                                 <i class="fa-solid fa-star"></i>
                             </div>
                             <div class="text_area_sobre_avaliacao">
-                                <div class="nome_perfil_sobre">Amanda Gomes</div>
-                                <div class="testemunho_perfil_sobre">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium illo ad voluptate sequi tenetur saepe commodi quidem iusto, vero tempore unde in aliquid, explicabo temporibus. Doloribus sit laudantium veniam earum!</div>
+                                <div class="nome_perfil_sobre">Lourdes Rodrigues</div>
+                                <div class="testemunho_perfil_sobre">Apola Artesanatos é onde a arte e a criatividade se encontram. Cada peça é feita com dedicação e carinho, trazendo beleza e exclusividade. Um verdadeiro testemunho de talento e tradição. Venha conhecer e se encantar!</div>
 
                             </div>
                         </div>
@@ -163,7 +174,7 @@ include "navbar_deslogado.php";
 
 
 
-        <section class="video_sobre">
+        <!-- <section class="video_sobre">
             <div class="content_video_sobre">
                 <h1 class="title_section_sobre">
                     Lorem ipsum dolor sit <span class="title_section_sobre2">amet consectetur</span>
@@ -176,17 +187,17 @@ include "navbar_deslogado.php";
                     <button class="btn_video_sobre">Confira</button>
                 </div>
             </div>
-        </section>
+        </section> -->
 
 
-        <section class="confira_sobre">
+        <!-- <section class="confira_sobre">
             <div class="content_confira_sobre">
                 <div class="text_confira_sobre">Lorem ipsum dolor sit amet</div>
                 <div class="text_confira_sobre">Lorem ipsum dolor sit </div>
                 <div class="content_btn_confira_sobre"><input placeholder="Envie seu e-mail" class="input_confira_sobre" type="email" name="" id=""> <button class="btn_confira_sobre">Enviar</button></div>
 
             </div>
-        </section>
+        </section> -->
     </main>
 <?php
 
