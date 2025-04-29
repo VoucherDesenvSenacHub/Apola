db10611298d7d74b8b0ce582c1ee0a28aaca8c84
lourdesdecdic <lourdesdecdic@gmail.com>
2025-04-29 07:14:15 -0400

    Subindo alterações no sobre nois

--- PI/Pages/user/SobreNos.php
+++ PI/Pages/user/SobreNos.php
@@ -1,41 +1,22 @@
 <?php
 
-
-require '../../App/config.inc.php';
-
-require '../../App/Session/Login.php';
-
 include "head.php";
-
-
-$result = Login::RequireLogout();
-
-
-if($result){
-    include 'navbar_logado.php';
-
-}else{
-    include 'navbar_deslogado.php';
-
-}
-
+include "navbar_deslogado.php";
 
 ?>
     <main style=" margin-bottom: unset;"  class="main2">
 
-        <section class="Banner_Sobre_Nos">
-            <div class="shape_sobre"></div> 
-            <div class="content_banner_sobre_nos"> 
-                <img class="img_sobre_banner" src="../../src/imagens/SobreMim/freepik-export-20241209130412wAw2.jpeg" alt="">
+        <!-- <section class="Banner_Sobre_Nos"> -->
+            <!-- <div class="shape_sobre"></div> -->
+            <!-- <div class="content_banner_sobre_nos"> -->
+                <!-- <img class="img_sobre_banner" src="../../src/imagens/SobreMim/freepik-export-20241209130412wAw2.jpeg" alt="">
                 <div class="content_coteudo_banner_sobre">
                     <h4 class="text_top_sobre">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
                     <h5 class="text_bottom_sobre">Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h4>
                     <button class="btn_sobre"> <a href="">Conheça</a></button>
                 </div>
             </div>
-        </section> 
-
-
+        </section> -->
 
         <section class="about_text">
             <div class="content_about_text">
@@ -44,9 +25,9 @@ if($result){
                 </div>
                 <div class="left_about_text">
                     <div class="title_text_about"> Sobre nós <span class="title_text_about2"> Apola Ecommerce Artesanatos </span></div>
-                    <div class="conteudo_text_about">
+                    <div class="conteudo_text_about">  
 
-                        <p class="texto_about">
+                        <p class="texto_about">  
                         A Apola Ecommerce Artesanatos nasceu para conectar artesãos talentosos a um público que valoriza a autenticidade e o trabalho manual.
                         Nossa loja virtual oferece uma curadoria especial de produtos artesanais, incluindo itens decorativos, joias, roupas, acessórios e utilidades domésticas, todos feitos com dedicação e qualidade por artesãos locais e regionais.
                         Nosso propósito é ampliar a visibilidade do artesanato, promovendo a cultura e incentivando o consumo consciente.
@@ -56,35 +37,38 @@ if($result){
                         <p class="texto_about">
                         Acreditamos que cada peça conta uma história e queremos levá-las até você. Siga-nos nas redes sociais e faça parte dessa comunidade apaixonada por arte e criatividade. Juntos, valorizamos o feito à mão!
                         </p>
+                        
+        
                     
                     </div>
                     <div class="content_number_dados_sobre">
+
                         <div class="couteudo_dados_sobre">
                             <div class="number_dados_sobre">28+</div>
                             <div class="text_dados_sobre">Apola Ecommerce Artesanatos</div>
                         </div>
+
                         <div class="couteudo_dados_sobre">
                             <div class="number_dados_sobre">894+</div>
                             <div class="text_dados_sobre">Apola Ecommerce Artesanatos</div>
                         </div>
+
                         <div class="couteudo_dados_sobre">
                             <div class="number_dados_sobre">42+</div>
                             <div class="text_dados_sobre">Apola Ecommerce Artesanatos</div>
                         </div>
 
-                    </div>
-                    <div class="content_btn_about">
+                    </div> 
+
+                    <!-- <div class="content_btn_about">
                         <button class="btn_about">Confira</button>
-                    </div>
+                    </div> -->
                 </div>
 
             </div>
         </section>
 
-
-
-
-        <section class="cards_service">
+        <!-- <section class="cards_service">
             <div class="content_cards_service">
                 <h1 class="title_section_sobre">
                     Lorem ipsum dolor sit <span class="title_section_sobre2">amet consectetur</span>
@@ -114,18 +98,129 @@ if($result){
                     </div>
                 </div>
             </div>
+        </section> -->
+
+        <!-- SEÇÃO DE COMENTÁRIOS DOS USUÁRIOS -->
+        <section class="comentarios-container content_card_avalicao_sobre"> <!-- Define o Formato do Container -->
+            <h2>O que dizem sobre nós</h2>
+
+            <!-- Slides do Carrossel -->
+            <div class="swiper_sobre">
+                <div class="swiper-wrapper">
+
+                    <!-- Card dos Comentários -->
+                    <div class="comentario swiper-slide">
+                        <!-- Foto da Lourdes -->
+                        <img src="../../src/imagens/sobre_nos/lourdes.jpg" alt="Foto de Lourdes Rodrigues" class="avatar">
+                        <p class="nome">Lourdes Rodrigues</p>
+                        <div class="comentario-texto">
+                            <p class="justificar-texto">Apola Artesanatos é onde a arte e a criatividade se encontram. Cada peça é feita com dedicação e carinho, trazendo beleza e exclusividade. Um verdadeiro testemunho de talento e tradição. <br> Simplesmente encantada com os produtos! A delicadeza de cada peça me emocionou. A entrega foi rápida, e o atendimento, impecável. Super recomendo! Venha conhecer e se encantar!</p>
+                        </div>
+
+                        <div class="content_star_sobre">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+
+                    <div class="comentario swiper-slide">
+                        <!-- Avatar fictício -->
+                        <img class="avatar" src="../../src/imagens/sobre_nos/avatar_carlos.png" alt="Carlos Henrique"/>
+                        <p class="nome">Carlos Henrique</p>
+                        <div class="comentario-texto">
+                        <p class="justificar-texto">Minha esposa adorou o presente que comprei aqui!<br>
+                            Parabéns pelo capricho e cuidado em cada detalhe!</p>
+                        </div>
+
+                        <div class="content_star_sobre">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+
+                    <div class="comentario swiper-slide">
+                        <!-- Avatar fictício -->
+                        <img src="../../src/imagens/sobre_nos/avatar_ana.png" alt="Ana Luiza" class="avatar">
+                        <p class="nome">Ana Luiza</p>
+                        <div class="comentario-texto">
+                        <p class="justificar-texto">Sou cliente há meses e cada nova compra me surpreende.<br>
+                            Vocês realmente sabem como encantar!</p>
+                        </div>
+                        <div class="content_star_sobre">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Setinhas do Carrossel -->
+            <div class="swiper-pagination-sobre"></div>
+            <div class="btn_card_sobre">
+                <div class="btn_prev_card_avaliacao"><i class="fa-solid default_btn_icon_card fa-chevron-left"></i></div>
+                <div class="btn_next_card_avaliacao"><i class="fa-solid default_btn_icon_card fa-chevron-right"></i></div>
+            </div>
         </section>
+    </main>
+<?php
+include "footer.php";
+?>
 
 
 
 
-        <section class="card_avaliacao_sobre">
-            <div class="content_card_avalicao_sobre">
+
+
+        <!-- <script src="../../src/JS/swiper_sobre.js"></script> -->
+
+        <!-- <section class="video_sobre">
+            <div class="content_video_sobre">
+                <h1 class="title_section_sobre">
+                    Lorem ipsum dolor sit <span class="title_section_sobre2">amet consectetur</span>
+                </h1>
+                <video  controls class="video_content">
+                    <source autoplay src="https://www.youtube.com/watch?v=kM8crupu9DM"> 
+                </video>
+                <div class="item_conteudo_sobre">
+                    <p class="text_sobre_video">Lorem ipsum dolor sit amet consectetur adipisicing elit. At quas fuga optio aspernatur laudantium minus, doloribus dolor dolore nihil nemo totam corrupti facilis beatae illo eum, excepturi atque nam rem?</p>
+                    <button class="btn_video_sobre">Confira</button>
+                </div>
+            </div>
+        </section> -->
+
+        <!-- <section class="confira_sobre">
+            <div class="content_confira_sobre">
+                <div class="text_confira_sobre">Lorem ipsum dolor sit amet</div>
+                <div class="text_confira_sobre">Lorem ipsum dolor sit </div>
+                <div class="content_btn_confira_sobre"><input placeholder="Envie seu e-mail" class="input_confira_sobre" type="email" name="" id=""> <button class="btn_confira_sobre">Enviar</button></div>
+
+            </div>
+        </section> -->
+    <!-- </main>
+
+            
+
+
+
+
+
+
+            <!-- <div class="content_card_avalicao_sobre">
                 <h1 class="title_section_sobre">
                 Apola Ecommerce Artesanatos <span class="title_section_sobre2"> </span>
                 </h1>
                 <div class="swiper_sobre">
                     <div class="swiper-wrapper">
+
                         <div class="swiper-slide">
                             <div class="content_img_perfil_sobre">
                                 <img src="../../src/imagens/cadastro/mulher-plana-cuidando-de-plantas.png" alt="">
@@ -143,6 +238,7 @@ if($result){
 
                             </div>
                         </div>
+
                         <div class="swiper-slide">
                             <div class="content_img_perfil_sobre">
                                 <img src="../../src/imagens/cadastro/mulher-plana-cuidando-de-plantas.png" alt="">
@@ -184,14 +280,16 @@ if($result){
                     <div class="btn_prev_card_avaliacao"><i class="fa-solid default_btn_icon_card fa-chevron-left"></i></div>
                     <div class="btn_next_card_avaliacao"><i class="fa-solid default_btn_icon_card fa-chevron-right"></i></div>
                 </div>
-            </div>
-        </section>
-        <script src="../../src/JS/swiper_sobre.js"></script>
+            </div> -->
+        <!-- </section> -->
 
 
 
 
-        <section class="video_sobre">
+
+        <!-- <script src="../../src/JS/swiper_sobre.js"></script> -->
+
+        <!-- <section class="video_sobre">
             <div class="content_video_sobre">
                 <h1 class="title_section_sobre">
                     Lorem ipsum dolor sit <span class="title_section_sobre2">amet consectetur</span>
@@ -204,22 +302,14 @@ if($result){
                     <button class="btn_video_sobre">Confira</button>
                 </div>
             </div>
-        </section> 
+        </section> -->
 
-
-        <section class="confira_sobre">
+        <!-- <section class="confira_sobre">
             <div class="content_confira_sobre">
                 <div class="text_confira_sobre">Lorem ipsum dolor sit amet</div>
                 <div class="text_confira_sobre">Lorem ipsum dolor sit </div>
                 <div class="content_btn_confira_sobre"><input placeholder="Envie seu e-mail" class="input_confira_sobre" type="email" name="" id=""> <button class="btn_confira_sobre">Enviar</button></div>
 
             </div>
-        </section> 
-    </main>
-<?php
-
-include "footer.php";
-
-
-
-?>
+        </section> -->
+    <!-- </main>-->
