3810fb53d4d4bde2921efaab3c8e24dfc364e3aa
Vitor-NLF <loureirovitornascimento@gmail.com>
2025-05-15 10:57:10 -0400

    Subindo algumas mudanças. Classe Banner.

--- PI/Pages/user/login.php
+++ PI/Pages/user/login.php
@@ -86,6 +86,7 @@ if(isset($_POST['logar'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../src/Css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <script src="../JS/validar_form_login.js" defer></script> -->
 
     <title>Login</title>
 </head> 
@@ -110,6 +111,7 @@ if(isset($_POST['logar'])){
                 <div class="container-login">
                     <div class="form-container">
                         <div class="text-login">Login</div>
+                        <form action="login.php" method="post">
                             <div class="form__group field">
                                 <input type="text" name='email' id="email-login" class="form__field" placeholder="E-mail" required>
                                 <label for="email" class="form__label">E-mail*</label>
