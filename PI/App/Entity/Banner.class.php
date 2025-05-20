7230474384296ac7ae29c386748d5a2a589ff8d3
Vitor-NLF <loureirovitornascimento@gmail.com>
2025-05-20 11:04:49 -0400

    Criação da Função no arquivo Banner.cass.php

--- PI/App/Entity/Banner.class.php
+++ PI/App/Entity/Banner.class.php
@@ -1,5 +1,17 @@
 <?php
+require_once '../DB/Database.php';
+
 
 class Banner{
-    public srting 
+    public srting $caminho;
+
+
+    public function cadastrarBanner(){
+        $db = new Database('banner');
+        $result = $db->insert(
+            [
+                'caminho' => $this->caminho,
+            ]
+            );
+    }
 }
