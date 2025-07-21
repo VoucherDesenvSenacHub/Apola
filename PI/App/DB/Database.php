2b4eca9fad748a2647b176b07e09ca5ed97887bb
Guilherme De Miranda <guilhermemiranda1080@gmail.com>
2025-07-21 15:27:03 -0400

    subindo o db que estava dando ero

--- PI/App/DB/Database.php
+++ PI/App/DB/Database.php
@@ -6,47 +6,31 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
 $dotenv->load();
 
 
-clASs Database{
-
-    // public $conection;
-    // public string $local = '10.38.0.125';
-    // public string $db = 'pi_artesanato';
-    // public string $user = 'devweb';
-    // public string $password = 'suporte@22';
-    // public $table;
-    
+class Database{
     public $conection;
-    public string $local;
-    public string $db;
-    public string $user;
-    public string $password;
+    public string $local="localhost";
+    public string $db="140p1";
+    public string $user = "devweb";
+    public string $password = "Suporte@22";
     public $table;
-
-
     
-
-    public function __construct($table = null) {
-        $this->local = $_ENV['DB_HOST'] ?? 'localhost';
-        $this->db = $_ENV['DB_DATABASE'] ?? 'Users';
-        $this->user = $_ENV['DB_USERNAME'] ?? 'root';
-        $this->password = $_ENV['DB_PASSWORD'] ?? '';
+    
+    public function __construct($table = null){
         $this->table = $table;
-        $this->conecta();
+        $result = $this->conecta();
     }
-        
-    // Função conectar com o banco de dados
+    
+    public function conecta(){
+        try {
+            $this->conection = new PDO("mysql:host=".$this->local.";dbname=$this->db",$this->user,$this->password); 
+            $this->conection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            // echo "Conectado com Sucesso!!";
+        } catch (PDOException $err) {
+            //retirar msg em produção
+            die("ERRO DE CONEXAO: " . $err->getMessage());
+        }
+    }
 
-    public function conecta() {
-    try {
-        $this->conection = new PDO("mysql:host=".$this->local.";dbname=$this->db",
-        $this->user, $this->password); 
-        $this->conection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $this->conection;  // <-- return the PDO object here!
-    } catch (PDOException $err) {
-        die("Connection Failed: " . $err->getMessage());
-        // no need to return false after die()
-    }
-}
 
 
     // Função para excutar uma função do banco de dados
