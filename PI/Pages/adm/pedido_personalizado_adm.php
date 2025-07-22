fecee93f47a16faa934c0b15d5ec8c13e2a89942
Guilherme De Miranda Cardoso <guilhermemiranda1080@gmail.com>
2025-07-22 10:21:14 -0400

    tirando o print r do detalhes do pedido personalizado

--- PI/Pages/adm/pedido_personalizado_adm.php
+++ PI/Pages/adm/pedido_personalizado_adm.php
@@ -13,7 +13,7 @@ if (!$id) {
 
 $entity = new Pedido();
 $pedido_cliente = $entity->buscar_pedidoperso_by_id($id);
-print_r($pedido_cliente);
+// print_r($pedido_cliente);
 
 if (!$pedido_cliente) {
     echo "Pedido personalizado não encontrado.";
