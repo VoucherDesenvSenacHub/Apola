88fedc05a18027aaa6b0a9676790bb8ce0bb6b46
DENTIN44 <silvaalvg@gmail.com>
2025-07-22 22:20:35 -0400

    new branch updated

---
+++ PI/App/Entity/require_once 'User.php
@@ -0,0 +1,103 @@
+require_once 'User.php';
+require_once(__DIR__ . '/../DB/Database.php');
+
+use App\DB\Database;
+
+
+require_once(__DIR__ . '/../DB/Database.php');
+
+use App\DB\Database;
+
+
+
+require_once 'Avaliacao.class.php';
+require_once(__DIR__ . '/../DB/Database.php');
+
+use App\DB\Database;
+
+
+
+use App\DB\Database;
+
+
+
+
+require_once(__DIR__ . '/../DB/Database.php');
+
+use App\DB\Database;
+use PDO;
+
+
+
+
+require_once 'User.php';
+require_once(__DIR__ . '/../DB/Database.php');
+
+use App\DB\Database;
+
+
+
+
+
+require_once(__DIR__ . '/../DB/Database.php');
+
+use App\DB\Database;
+
+
+
+
+require_once __DIR__ . '/../DB/Database.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+// rest of your PasswordReset class/code
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
+use App\DB\Database;
+
+
+
+$root = realpath(__DIR__ . '/../../../'); // goes from App/Entity to Apola/
+if ($root === false) {
+    throw new Exception("Could not find the root path for .env");
+}
+
+$dotenv = Dotenv::createImmutable($root);
+$dotenv->load();
+
+
+
+
+use App\DB\Database;
+
+
+
+
+// PI/App/Entity/ProductModel.php
+namespace App\Entity;
+require_once __DIR__ . '/../DB/Database.php';
+
+use App\Entity\Product;
+use App\Entity\Category;
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+
+
+use App\DB\Database;
+
+
+
+
+
+require_once 'Pedido.class.php';
+
+use App\DB\Database;
+
+
+
+require_once(__DIR__ . '/../DB/Database.php');
+
+use App\DB\Database;
